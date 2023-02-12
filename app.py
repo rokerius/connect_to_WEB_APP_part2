36360153df2c1a8f9151b4b19e773e42f0bbea65
Den <rokerius@gmail.com>
2023-02-12 15:30:26 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_info.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # Устанавливается по умолчанию
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        # try:
+        db.session.add(article)
+        db.session.commit()
+        return redirect('/')
+        # except:
+        #     return "При добавлении произошла ошибка"
+    else:
+        return render_template("create-article.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
