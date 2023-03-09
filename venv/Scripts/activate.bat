9702b3d70df6a652eef2b4d0ebf11178f6652fee
Den <rokerius@gmail.com>
2023-03-09 23:54:37 +0300

    NEW CENTURY

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\Connection_to_WEB_APP\venv
+set VIRTUAL_ENV=F:\Poker_connection_to_WEB_APP\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
